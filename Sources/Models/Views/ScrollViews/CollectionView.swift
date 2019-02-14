f7c3534656e1fde3685e3fcf7e0c3f137aaa6272
Pierre Felgines <pierre.felgines@fabernovel.com>
2019-02-14 18:05:12 +0100

    Create CollectionViewContentView struct
    This commit fixes #48

--- Sources/Models/Views/ScrollViews/CollectionView.swift
+++ Sources/Models/Views/ScrollViews/CollectionView.swift
@@ -122,7 +122,7 @@ public struct CollectionViewCell: IBDecodable, ViewProtocol, IBReusable {
     public let autoresizingMask: AutoresizingMask?
     public let clipsSubviews: Bool?
     public let constraints: [Constraint]?
-    public let contentView: View
+    public let contentView: CollectionViewContentView
     public let contentMode: String?
     public let customClass: String?
     public let customModule: String?
@@ -160,6 +160,76 @@ public struct CollectionViewCell: IBDecodable, ViewProtocol, IBReusable {
             children += elements as [IBElement]
         }
         return children
+    }
+
+    public struct CollectionViewContentView: IBDecodable, ViewProtocol {
+
+        public let id: String = ""
+        public let elementClass: String = "UIView"
+
+        public let key: String?
+        public let autoresizingMask: AutoresizingMask?
+        public let clipsSubviews: Bool?
+        public let constraints: [Constraint]?
+        public let contentMode: String?
+        public let customClass: String?
+        public let customModule: String?
+        public let customModuleProvider: String?
+        public let userLabel: String?
+        public let colorLabel: String?
+        public let isMisplaced: Bool?
+        public let isAmbiguous: Bool?
+        public let opaque: Bool?
+        public let rect: Rect?
+        public let subviews: [AnyView]?
+        public let translatesAutoresizingMaskIntoConstraints: Bool?
+        public let userInteractionEnabled: Bool?
+        public let viewLayoutGuide: LayoutGuide?
+        public let userDefinedRuntimeAttributes: [UserDefinedRuntimeAttribute]?
+        public let connections: [AnyConnection]?
+        public let variations: [Variation]?
+
+        enum ConstraintsCodingKeys: CodingKey { case constraint }
+        enum VariationCodingKey: CodingKey { case variation }
+
+        static func decode(_ xml: XMLIndexerType) throws -> CollectionViewContentView {
+            let container = xml.container(keys: MappedCodingKey.self).map { (key: CodingKeys) in
+                let stringValue: String = {
+                    switch key {
+                    case .isMisplaced: return "misplaced"
+                    case .isAmbiguous: return "ambiguous"
+                    default: return key.stringValue
+                    }
+                }()
+                return MappedCodingKey(stringValue: stringValue)
+            }
+            let constraintsContainer = container.nestedContainerIfPresent(of: .constraints, keys: ConstraintsCodingKeys.self)
+            let variationContainer = xml.container(keys: VariationCodingKey.self)
+
+            return CollectionViewContentView(
+                key:                                       container.attributeIfPresent(of: .key),
+                autoresizingMask:                          container.elementIfPresent(of: .autoresizingMask),
+                clipsSubviews:                             container.attributeIfPresent(of: .clipsSubviews),
+                constraints:                               constraintsContainer?.elementsIfPresent(of: .constraint),
+                contentMode:                               container.attributeIfPresent(of: .contentMode),
+                customClass:                               container.attributeIfPresent(of: .customClass),
+                customModule:                              container.attributeIfPresent(of: .customModule),
+                customModuleProvider:                      container.attributeIfPresent(of: .customModuleProvider),
+                userLabel:                                 container.attributeIfPresent(of: .userLabel),
+                colorLabel:                                container.attributeIfPresent(of: .colorLabel),
+                isMisplaced:                               container.attributeIfPresent(of: .isMisplaced),
+                isAmbiguous:                               container.attributeIfPresent(of: .isAmbiguous),
+                opaque:                                    container.attributeIfPresent(of: .opaque),
+                rect:                                      container.elementIfPresent(of: .rect),
+                subviews:                                  container.childrenIfPresent(of: .subviews),
+                translatesAutoresizingMaskIntoConstraints: container.attributeIfPresent(of: .translatesAutoresizingMaskIntoConstraints),
+                userInteractionEnabled:                    container.attributeIfPresent(of: .userInteractionEnabled),
+                viewLayoutGuide:                           container.elementIfPresent(of: .viewLayoutGuide),
+                userDefinedRuntimeAttributes:              container.childrenIfPresent(of: .userDefinedRuntimeAttributes),
+                connections:                               container.childrenIfPresent(of: .connections),
+                variations:                                variationContainer.elementsIfPresent(of: .variation)
+            )
+        }
     }
 
     enum ConstraintsCodingKeys: CodingKey { case constraint }
