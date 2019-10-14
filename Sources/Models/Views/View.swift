a1ce5508ff4f7bba53d35fbf47ecf19c4efd36f6
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-10-14 15:44:44 +0200

    Add missing elements
    As required in #33:
    - Adding `inset` in view
    - Adding `edgeInsets` in tableViewContentView

--- Sources/Models/Views/View.swift
+++ Sources/Models/Views/View.swift
@@ -36,6 +36,7 @@ public struct View: IBDecodable, ViewProtocol, IBIdentifiable {
     public let backgroundColor: Color?
     public let tintColor: Color?
     public let hidden: Bool?
+    public let inset: Inset?
 
     enum ConstraintsCodingKeys: CodingKey { case constraint }
     enum VariationCodingKey: CodingKey { case variation }
@@ -83,7 +84,8 @@ public struct View: IBDecodable, ViewProtocol, IBIdentifiable {
             variations:                                variationContainer.elementsIfPresent(of: .variation),
             backgroundColor:                           colorsContainer?.withAttributeElement(.key, CodingKeys.backgroundColor.stringValue),
             tintColor:                                 colorsContainer?.withAttributeElement(.key, CodingKeys.tintColor.stringValue),
-            hidden:                                    container.attributeIfPresent(of: .hidden)
+            hidden:                                    container.attributeIfPresent(of: .hidden),
+            inset:                                     container.elementIfPresent(of: .inset)
         )
     }
 }
