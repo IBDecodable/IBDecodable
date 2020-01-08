1867774adf658b7210802e34555b240a31417aa0
Alex Petuschak <alex@swiftify.com>
2020-01-08 19:16:40 +0200

    + Support in-memory parsing of Storyboard and XIB documents (added initializers that accept `xml` as a String parameter)

--- Sources/Models/Files/StoryboardFile.swift
+++ Sources/Models/Files/StoryboardFile.swift
@@ -19,22 +19,31 @@ public class StoryboardFile: InterfaceBuilderFile {
         self.pathString = path
         self.document = try type(of: self).parseContent(pathString: path)
     }
-
+    
     public init(url: URL) throws {
         self.pathString = url.absoluteString
         self.document = try type(of: self).parseContent(url: url)
     }
-
-    private static func parseContent(pathString: String) throws -> StoryboardDocument {
-        let parser = InterfaceBuilderParser()
-        let content = try String(contentsOfFile: pathString)
-        return try parser.parseDocument(xml: content)
+    
+    public init(xml: String) throws {
+        self.pathString = "$temporary.storyboard"
+        self.document = try type(of: self).parseContent(xml: xml)
     }
 
+    private static func parseContent(pathString: String) throws -> StoryboardDocument {
+        let content = try String(contentsOfFile: pathString)
+        return try parseContent(xml: content)
+    }
+    
     private static func parseContent(url: URL) throws -> StoryboardDocument {
         let parser = InterfaceBuilderParser()
         let content = try Data(contentsOf: url)
         return try parser.parseDocument(data: content)
+    }
+    
+    private static func parseContent(xml: String) throws -> StoryboardDocument {
+        let parser = InterfaceBuilderParser()
+        return try parser.parseDocument(xml: xml)
     }
 
 }
