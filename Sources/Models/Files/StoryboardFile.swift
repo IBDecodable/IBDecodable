397e21b3e2af504bbae402535857628be4a7280b
Alex Petuschak <alex@swiftify.com>
2020-01-09 17:34:52 +0200

    * Fixed PR comments

--- Sources/Models/Files/StoryboardFile.swift
+++ Sources/Models/Files/StoryboardFile.swift
@@ -19,12 +19,12 @@ public class StoryboardFile: InterfaceBuilderFile {
         self.pathString = path
         self.document = try type(of: self).parseContent(pathString: path)
     }
-    
+
     public init(url: URL) throws {
         self.pathString = url.absoluteString
         self.document = try type(of: self).parseContent(url: url)
     }
-    
+
     public init(xml: String) throws {
         self.pathString = "$temporary.storyboard"
         self.document = try type(of: self).parseContent(xml: xml)
@@ -34,13 +34,13 @@ public class StoryboardFile: InterfaceBuilderFile {
         let content = try String(contentsOfFile: pathString)
         return try parseContent(xml: content)
     }
-    
+
     private static func parseContent(url: URL) throws -> StoryboardDocument {
         let parser = InterfaceBuilderParser()
         let content = try Data(contentsOf: url)
         return try parser.parseDocument(data: content)
     }
-    
+
     private static func parseContent(xml: String) throws -> StoryboardDocument {
         let parser = InterfaceBuilderParser()
         return try parser.parseDocument(xml: xml)
